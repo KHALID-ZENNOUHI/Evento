f177ca686fb2f9dcf803760c712828ff64f86328
KHALID-ZENNOUHI <khalidzennouhi08@gmail.com>
2024-03-04 16:53:30 +0100

    add docker to the project

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,252 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<!DOCTYPE html>
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+<!--
+ // WEBSITE: https://themefisher.com
+ // TWITTER: https://twitter.com/themefisher
+ // FACEBOOK: https://www.facebook.com/themefisher
+ // GITHUB: https://github.com/themefisher/
+-->
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Galaxy - Personal Blog Template</title>
+
+    <!-- mobile responsive meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- ** Plugins Needed for the Project ** -->
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="plugins/fontawesome/css/all.css">
+
+    <!-- Main Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!--Favicon-->
+    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+    <link rel="icon" href="images/favicon.png" type="image/x-icon">
+
+</head>
+<body>
+    <!-- START preloader-wrapper -->
+    <div class="preloader-wrapper">
+        <div class="preloader-inner">
+            <div class="spinner-border text-red"></div>
         </div>
+    </div>
+    <!-- END preloader-wrapper -->
+    
+    <!-- START main-wrapper -->
+    <section class="d-flex">
+  
+<!-- start of sidenav -->
+<aside><div class="sidenav position-sticky d-flex flex-column justify-content-between">
+    <a class="navbar-brand" href="index.html" class="logo">
+        <img src="images/logo.png" alt="">
+    </a>
+    <!-- end of navbar-brand -->
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+    <div class="navbar navbar-dark my-4 p-0 font-primary">
+        <ul class="navbar-nav w-100">
+            <li class="nav-item ">
+                <a class="nav-link text-white px-0 pt-0" href="index.html">Home</a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link text-white px-0" href="about.html">About</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link text-white px-0" href="contact.html">Contact</a>
+            </li>
+            <li class="nav-item  accordion">
+                <div id="drop-menu" class="drop-menu collapse">
+                    <a class="d-block " href="index-2.html">Home 2</a>
+                    <a class="d-block " href="category.html">Category</a>
+                    <a class="d-block " href="post-details.html">Post Details</a>
+                    <a class="d-block " href="privacy.html">Privacy &amp; Policy</a>
+                </div>
+                <a class="nav-link text-white" href="#!" role="button" data-toggle="collapse" data-target="#drop-menu" aria-expanded="false" aria-controls="drop-menu">Pages</a>
+            </li>
+            <li class="nav-item mt-3">
+                <select class="custom-select bg-transparent rounded-0 text-white shadow-none" id="pick-lang">
+                    <option selected value="en">English</option>
+                    <option value="fr">French</option>
+                </select>
+            </li>
+        </ul>
+    </div>
+    <!-- end of navbar -->
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+    <ul class="list-inline nml-2">
+        <li class="list-inline-item">
+            <a href="#!" class="text-white text-red-onHover pr-2">
+                <span class="fab fa-twitter"></span>
             </a>
+        </li>
+        <li class="list-inline-item">
+            <a href="#!" class="text-white text-red-onHover p-2">
+                <span class="fab fa-facebook-f"></span>
+            </a>
+        </li>
+        <li class="list-inline-item">
+            <a href="#!" class="text-white text-red-onHover p-2">
+                <span class="fab fa-instagram"></span>
+            </a>
+        </li>
+        <li class="list-inline-item">
+            <a href="#!" class="text-white text-red-onHover p-2">
+                <span class="fab fa-linkedin-in"></span>
+            </a>
+        </li>
+    </ul>
+    <!-- end of social-links -->
+</div></aside>
+<!-- end of sidenav -->
+    <div class="main-content">
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+        <!-- start of mobile-nav -->
+<header class="mobile-nav pt-4">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-6">
+                <a href="index.html">
+                    <img src="images/logo.png" alt="">
+                </a>
+            </div>
+            <div class="col-6 text-right">
+                <button class="nav-toggle bg-transparent border text-white">
+                    <span class="fas fa-bars"></span>
+                </button>
+            </div>
         </div>
-    </form>
-</x-guest-layout>
+    </div>
+</header>
+<div class="nav-toggle-overlay"></div>
+<!-- end of mobile-nav -->
+
+        <div class="container py-4 my-5">
+            <div class="row">
+    <div class="col-lg-5 col-md-8">
+        <form class="search-form" action="#">
+            <div class="input-group">
+                <input type="search" class="form-control bg-transparent shadow-none rounded-0" placeholder="Search here">
+                <div class="input-group-append">
+                    <button class="btn" type="submit">
+                        <span class="fas fa-search"></span>
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+            <div class="row">
+                <div class="col-md-10">
+                    <div class="contact-form bg-dark">
+                        <h1 class="text-white add-letter-space mb-5">Rgister</h1>
+                        <form method="POST" class="needs-validation" novalidate>
+                            @csrf
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group mb-5">
+                                        <label for="name" class="text-black-300">Your Name</label>
+                                        <input type="text" id="firstName" name="name" :value="old('name')" class="form-control bg-transparent rounded-0 border-bottom shadow-none pb-15 px-0" placeholder="Robert Lee" required>
+                                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                                        <p class="invalid-feedback">Your first-name is required!</p>
+                                    </div>
+                                </div>
+                                
+                                <div class="col-md-6">
+                                    <div class="form-group mb-5">
+                                        <label for="email" class="text-black-300">E-Mail Address</label>
+                                        <input type="email" id="email" name="email" :value="old('email')" class="form-control bg-transparent rounded-0 border-bottom shadow-none pb-15 px-0" placeholder="email" required>
+                                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                        <p class="invalid-feedback">Your email is required!</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group mb-5">
+                                        <label for="password" class="text-black-300">Your Password</label>
+                                        <input type="password" id="lastName" name="password" class="form-control bg-transparent rounded-0 border-bottom shadow-none pb-15 px-0" placeholder="password" required>
+                                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                        <p class="invalid-feedback">Your password is required!</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group mb-5">
+                                        <label for="password_confirmation" class="text-black-300">Your Password confirmation</label>
+                                        <input type="password" name="password_confirmation" id="lastName" class="form-control bg-transparent rounded-0 border-bottom shadow-none pb-15 px-0" placeholder="confirmation_password" required>
+                                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                                        <p class="invalid-feedback">Your confirmation password is required!</p>
+                                    </div>
+                                </div>
+                                
+                                <div class="col-md-12">
+                                    <button type="submit" class="btn btn-sm btn-primary">Send Now <img src="images/arrow-right.png" alt=""></button>
+                                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+                                        {{ __('Already registered?') }}
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- start of footer -->
+<footer class="bg-dark">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-lg-3 col-sm-6 mb-5">
+                <h5 class="font-primary text-white mb-4">Inspirations</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#!">Privacy State</a></li>
+                    <li><a href="#!">Privacy</a></li>
+                    <li><a href="#!">State</a></li>
+                    <li><a href="#!">Privacy</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-sm-6 mb-5">
+                <h5 class="font-primary text-white mb-4">Templates</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#!">Privacy State</a></li>
+                    <li><a href="#!">Privacy</a></li>
+                    <li><a href="#!">State</a></li>
+                    <li><a href="#!">Privacy</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-sm-6 mb-5">
+                <h5 class="font-primary text-white mb-4">Resource</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#!">Privacy State</a></li>
+                    <li><a href="#!">Privacy</a></li>
+                    <li><a href="#!">State</a></li>
+                    <li><a href="#!">Privacy</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-sm-6 mb-5">
+                <h5 class="font-primary text-white mb-4">Company</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#!">Privacy State</a></li>
+                    <li><a href="#!">Privacy</a></li>
+                    <li><a href="#!">State</a></li>
+                    <li><a href="#!">Privacy</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- end of footer -->
+    </div>
+    </section>
+    <!-- END main-wrapper -->
+
+    <!-- All JS Files -->
+    <script src="plugins/jQuery/jquery.min.js"></script>
+    <script src="plugins/bootstrap/bootstrap.min.js"></script>
+
+    <!-- Main Script -->
+    <script src="js/script.js"></script>
+</body>
+</html>
